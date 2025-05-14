a774f467d3bc86ab6b5aff545217a6127a52aa22
donghyun-1 <20dkwak@gmail.com>
2025-05-14 21:28:01 +0900

    5/14 : 이친수

---
+++ Baekjoon/donghyun-1/Dynamic_Programming/2193.cpp
@@ -0,0 +1,16 @@
+// https://www.acmicpc.net/problem/2193
+// 이친수 (실버 3)
+// 2025-05-14
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+
+
+    return 0;
+}
